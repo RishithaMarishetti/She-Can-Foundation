4829f3532f4a78752111d16b2e53b66a52e28fa9
Rishitha Marishetti <rishithamarishetti25@gmail.com>
2026-05-26 20:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage(){
+
+    alert("Thank you for supporting She Can Foundation!");
+
+}
+
+function scrollToAbout(){
+
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
